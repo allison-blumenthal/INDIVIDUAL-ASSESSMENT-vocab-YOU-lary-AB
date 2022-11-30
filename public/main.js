dd70863fd81b48ccbba9ce40d985774f52a96ad8
Allison Blumenthal <blumen.allison@gmail.com>
2022-11-29 20:26:11 -0500

    added message on DOM for login screen

--- public/main.js
+++ public/main.js
@@ -4,6 +4,11 @@ import 'bootstrap'; // import bootstrap elements and js
 import '../styles/main.scss';
 
 const init = () => {
+  document.querySelector('#app').innerHTML = `
+  <h1>Welcome to Vocab-YOU-lary!</h1>
+  <h5>Where YOU make the vocab that counts.</h5>
+  `;
+
   ViewDirectorBasedOnUserAuthStatus();
 };
 
