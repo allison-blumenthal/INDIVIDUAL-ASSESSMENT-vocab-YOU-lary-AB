4684227a150d92f46b882636fc58fd7772155909
Allison Blumenthal <blumen.allison@gmail.com>
2022-12-02 20:11:25 -0500

    functional delete button

--- events/domEvents.js
+++ events/domEvents.js
@@ -2,12 +2,11 @@ import { getVocab, deleteVocab } from '../api/vocabData';
 import { showVocab } from '../pages/vocabCards';
 
 const domEvents = (user) => {
-  document.querySelector('#main-container').addEventListener('click', (e) =>
-  {
-    // click event for deleting a vocab card
+  document.querySelector('#main-container').addEventListener('click', (e) => {
+  // click event for deleting a vocab card
     if (e.target.id.includes('delete-vocab')) {
+      // eslint-disable-next-line no-alert
       if (window.confirm('Delete this card?')) {
-        console.warn('clicked delete card', e.target.id);
         const [, firebaseKey] = e.target.id.split('--');
 
         deleteVocab(firebaseKey).then(() => {
--- utils/startApp.js
+++ utils/startApp.js
@@ -1,11 +1,13 @@
 import logoutButton from '../components/buttons/logoutButton';
 import domBuilder from '../components/shared/domBuilder';
 import navBar from '../components/shared/navBar';
-import getVocab from '../api/vocabData';
+import { getVocab } from '../api/vocabData';
 import { showVocab } from '../pages/vocabCards';
+import domEvents from '../events/domEvents';
 
 const startApp = (user) => {
   domBuilder(user);
+  domEvents(user);
   navBar();
   logoutButton();
 
