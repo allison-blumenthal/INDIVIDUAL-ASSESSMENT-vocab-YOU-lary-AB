1bde7cb928ea43f2d25879bdeb0fabf0a7aa7e4b
Allison Blumenthal <blumen.allison@gmail.com>
2022-12-03 14:31:10 -0500

    got filter buttons to work

--- components/forms/addVocabForm.js
+++ components/forms/addVocabForm.js
@@ -7,21 +7,23 @@ const addVocabForm = (obj = {}) => {
   const domString = `
   <form id="${obj.firebaseKey ? `update-vocab--${obj.firebaseKey}` : 'submit-vocab'}" class="form-floating">
     <div class="form-group">
-      <input type="text" class="form-control" id="term" aria-describedby="vocabTerm"  placeholder="Enter Vocab Term" value="${obj.term || ''}" required>
       <label for="term">Vocab Term</label>
+      <input type="text" class="form-control" id="term" aria-describedby="vocabTerm"  placeholder="Enter Vocab Term" value="${obj.term || ''}" required>
     </div>
+
     <div class="form-group">
+      <label for="category">Tech/Language Category</label>
       <select id="category" name="vocabCategory" value="${obj.category || ''}" required>
         <option value="HTML">HTML</option>
         <option value="CSS">CSS</option>
         <option value="JavaScript">JavaScript</option>
+        <option value="General Tech">General Tech</option>
       </select>
-      <label for="category">Vocab Category</label>
     </div>
 
     <div class="form-group">
-      <input type="text" class="form-control" id="definition" aria-describedby="vocabDefinition"  placeholder="Enter Vocab Definition" value="${obj.definition || ''}" required>
       <label for="definition">Vocab Definition</label>
+      <input type="text" class="form-control" id="definition" aria-describedby="vocabDefinition"  placeholder="Enter Vocab Definition" value="${obj.definition || ''}" required>
     </div>
     <button type="submit" class="btn btn-primary">Submit Vocab
     </button>
---
+++ events/filterBtnEvents.js
@@ -0,0 +1,24 @@
+import {
+  getHTMLVocab, getCSSVocab, getJSVocab, getTechVocab
+} from '../api/vocabData';
+import { showVocab } from '../pages/vocabCards';
+
+const filterBtnEvents = (user) => {
+  document.querySelector('#html-button').addEventListener('click', () => {
+    getHTMLVocab(user.uid).then(showVocab);
+  });
+
+  document.querySelector('#css-button').addEventListener('click', () => {
+    getCSSVocab(user.uid).then(showVocab);
+  });
+
+  document.querySelector('#js-button').addEventListener('click', () => {
+    getJSVocab(user.uid).then(showVocab);
+  });
+
+  document.querySelector('#tech-button').addEventListener('click', () => {
+    getTechVocab(user.uid).then(showVocab);
+  });
+};
+
+export default filterBtnEvents;
